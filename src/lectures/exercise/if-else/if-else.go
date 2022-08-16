62be2eb246f8a5573f6396238e0bef9e1a56c067
Catarina Carreiro <110242229+carreir@users.noreply.github.com>
2022-08-16 12:37:30 +0100

    exercise if-else

--- src/lectures/exercise/if-else/if-else.go
+++ src/lectures/exercise/if-else/if-else.go
@@ -45,9 +45,27 @@ func accessDenied() {
 	fmt.Println("Denied")
 }
 
+func weekday(day int) bool {
+	return day <= 4
+}
+
 func main() {
 	// The day and role. Change these to check your work.
 	today, role := Tuesday, Guest
 
-	accessGranted()
+	if role == Admin || role == Manager {
+		accessGranted()
+	} else if role == Contractor && !weekday(today) {
+		accessGranted()
+	} else if role == Member && weekday(today) {
+		accessGranted()
+	} else if role == Guest && (today == Monday ||
+		today == Wednesday ||
+		today == Friday) {
+		accessGranted()
+
+	} else {
+		accessDenied()
+	}
+
 }
